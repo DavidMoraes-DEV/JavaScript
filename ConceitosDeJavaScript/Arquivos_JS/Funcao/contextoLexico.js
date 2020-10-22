edcf0933d74830aadd7e2d8da1774de306033572
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-21 23:56:52 -0300

    Funcao: Contexto lexico

---
+++ ConceitosDeJavaScript/Arquivos_JS/Funcao/contextoLexico.js
@@ -0,0 +1,32 @@
+const valor = 'Global'
+
+function minhaFuncao() {
+    console.log(valor)
+}
+
+function exec() {
+    const valor = 'Local'
+    minhaFuncao()
+}
+
+exec()
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
