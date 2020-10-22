157882fc1fc87c1f0063b172dbc56944414ce4fe
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-22 18:25:24 -0300

    Array: Funcao Reduce parte 01

---
+++ ConceitosDeJavaScript/Arquivos_JS/Array/FuncaoReduce01.js
@@ -0,0 +1,36 @@
+const alunos = [
+    { nome: 'Joao', nota: 5, bolsista: false },
+    { nome: 'Maria', nota: 10, bolsista: true },
+    { nome: 'Pedro', nota: 6, bolsista: false },
+    { nome: 'Ana', nota: 8, bolsista: true }
+]
+
+console.log(alunos.map(a => a.nota))
+
+console.log()
+
+//A função .reduce pega o primeira elemento e acumula com o segundo elemento, depois pega o resultado e acumula com o próximo elemento e assim por diante, conforme exemplo:
+const resultado = alunos.map(a => a.nota).reduce(function (acumulador, atual) {
+    console.log(acumulador, atual)
+    return acumulador + atual
+})
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
