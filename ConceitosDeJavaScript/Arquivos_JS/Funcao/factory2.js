9366178d2b4b862c2237bdffbec6719eb340725b
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-21 23:59:18 -0300

    Funcao: Factory parte 02

---
+++ ConceitosDeJavaScript/Arquivos_JS/Funcao/factory2.js
@@ -0,0 +1,30 @@
+function criarProduto(nome, preco) {
+    return {
+        nome,
+        preco,
+        desconto: 0.1
+    }
+}
+
+console.log(criarProduto('Notebook', 2199.49))
+console.log(criarProduto('iPad', 1199.49))
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
