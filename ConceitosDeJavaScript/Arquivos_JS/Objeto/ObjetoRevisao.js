a17af06a2a62675a1c9c014f3834cf20c97a0dea
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-22 00:09:04 -0300

    Objeto: Revisao Basica

---
+++ ConceitosDeJavaScript/Arquivos_JS/Objeto/ObjetoRevisao.js
@@ -0,0 +1,67 @@
+//Coleção dinâmica de pares chave/valor
+const produto = new Object
+
+produto.name - "Cadeira"
+produto["MarcaDoProduto"] = "Generica"
+produto.preco = 220.50
+
+console.log(produto)
+delete produto.preco
+delete produto["MarcaDoProduto"]
+console.log(produto)
+
+const carro = {
+    modelo: "A4",
+    valor: 89000,
+    proprietario: {
+        nome: "Raul",
+        idade: 56,
+        endereco: {
+            logradouro: "Rua ABC",
+            numero: 123
+        }
+    },
+    condutores: [{
+        nome: "Junior",
+        idade: 19
+    }, {
+        nome: "Ana",
+        idade: 42
+    }],
+    calcularValorSeguro: function() {
+        // ...
+    }
+}
+
+carro.proprietario.endereco.numero = 1000
+carro["proprietario"]["endereco"]["logradouro"] = "Av Gigante"
+
+console.log(carro)
+
+delete carro.condutores
+delete carro.proprietario.endereco
+delete carro.calcularValorSeguro
+
+console.log("\n\n")
+console.log(carro)
+
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
