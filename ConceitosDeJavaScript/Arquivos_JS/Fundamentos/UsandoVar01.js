74163da4827d72aab7a7e4f9e7434facbdf49e4d
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-21 22:27:04 -0300

    Fundamentos: Usando Var parte 01

---
+++ ConceitosDeJavaScript/Arquivos_JS/Fundamentos/UsandoVar01.js
@@ -0,0 +1,40 @@
+{
+    {
+        {
+            {
+                var mensagem = "Sera?\n"
+                console.log(mensagem) //Em outras linguagens a variável mensagem seria visivel apenas dentro do escopo em que ela foi declarada
+            }
+        }
+    }
+}
+
+console.log(mensagem) //Mas no caso da linguagem js a variáveç do tipo var ficará visivel em todo o programa mesmo que for forá do escopo ao qual ela foi declarada
+
+function teste(){
+    var local = 123
+    console.log(local) //No caso de uma variável declarada dentro de uma função qualquer, essa variável será acessada apenas dentro do escopo da função
+}
+
+teste()
+//console.log(local) Fora do escopo da função será gerado um erro do tipo: ReferenceError
+
+
+
+
+/*
+================================================================
+=---------------------- FINALIZAR CONSOLE ---------------------=
+================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
