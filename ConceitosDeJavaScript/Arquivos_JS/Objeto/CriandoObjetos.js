242724b303223e5f63f35ea1f4ef15bce89c7e0a
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-22 00:10:36 -0300

    Objeto: Criando Objeto

---
+++ ConceitosDeJavaScript/Arquivos_JS/Objeto/CriandoObjetos.js
@@ -0,0 +1,69 @@
+//Usando a notação literal
+const obj1 = {}
+
+console.log(obj1)
+
+//Object em JS
+const obj2 = new Object
+console.log(typeof Object, typeof new Object)
+console.log(obj2)
+
+//Funções Construtoras
+function Produto(nome, preco, desc){
+    this.nome = nome
+    this.getPrecoComDesconto = () => {
+        return preco * (1-desc)
+    }
+}
+
+const p1 = new Produto("Caneta", 7.99, 0.15)
+const p2 = new Produto("Notebook", 2998.99, 0.25)
+
+console.log(p1.getPrecoComDesconto(), p2.getPrecoComDesconto())
+
+//Função Factory
+function criarFuncionario(nome, salarioBase, faltas) {
+    return {
+        nome,
+        salarioBase,
+        faltas,
+        getSalario(){
+            return (salarioBase / 30) * (30 - faltas)
+        }
+    }
+}
+
+const f1 = criarFuncionario("João", 7980, 4)
+const f2 = criarFuncionario("Maria", 11400, 1)
+
+console.log(f1.getSalario(), f2.getSalario())
+
+// Object.create
+const filha = Object.create(null)
+filha.nome = "Ana"
+console.log(filha)
+
+// Uma função famosa que retorna Objeto...
+const fromJSON = JSON.parse('{"info": "Sou um JSON"}')
+console.log(fromJSON.info)
+
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
---
+++ ConceitosDeJavaScript/Arquivos_JS/Objeto/OO.js
@@ -0,0 +1,24 @@
+//CÓDIGO NÃO EXECUTÁVEL
+
+//Procedural
+processamento(valor1, valor2, valor3) //Uma função identificada por um nome que manipula dados que chega como parâmetro
+
+//OO
+objeto = { //Um objeto que possui valores e tem funções internamente. Então o objeto funciona como uma "capsula" que engloba comportamentos e valores
+    valor1,
+    valor2,
+    valor3,
+    processamento(){
+        //...
+    }
+}
+objeto.processamento() // O foco em OO passou a ser o objeto e antes em procedural era a função
+
+/**
+ * Principios Importantes:
+ * 1. Abstração
+ * 2. Encapsulamento
+ * 3. Herança (Prototype)
+ * 4. Polimorfismo
+ */
+
