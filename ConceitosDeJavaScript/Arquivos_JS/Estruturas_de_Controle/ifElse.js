e5caba1d9e18d6b9aa77c13c79f0778373067357
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-21 23:08:17 -0300

    Estruturas de COntrole: If Else

---
+++ ConceitosDeJavaScript/Arquivos_JS/Estruturas_de_Controle/ifElse.js
@@ -0,0 +1,31 @@
+const imprimirResultado = function(nota) {
+    if(nota >= 7) {
+        console.log('Aprovado!')
+    } else {
+        console.log('Reprovado!')
+    }
+}
+
+imprimirResultado(10)
+imprimirResultado(4)
+imprimirResultado('Epa!') // cuidado!!!
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
