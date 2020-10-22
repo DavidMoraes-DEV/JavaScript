36c3529599b8573ceb334a06dd5d48ba26ea6814
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-21 22:13:30 -0300

    Fundamentos: Math

---
+++ ConceitosDeJavaScript/Arquivos_JS/Fundamentos/Math.js
@@ -0,0 +1,27 @@
+console.log()
+
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2) //Math é um objeto que possui funções matemáticas
+
+console.log(area)
+console.log(typeof Math)
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
