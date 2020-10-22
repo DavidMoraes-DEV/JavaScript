3ea94c37d79669b3ac97b29925b1c4694678e224
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-22 00:12:14 -0300

    Objetos: Constantes

---
+++ ConceitosDeJavaScript/Arquivos_JS/Objeto/ObjetosConstantes.js
@@ -0,0 +1,37 @@
+// pessoa -> 123 -> {...}
+const pessoa = {nome: "Joao"}
+pessoa.nome = "Pedro"
+
+console.log(pessoa)
+
+// pessoa -> 456 -> {...}
+//pessoa = {nome: "Ana"} => Erro
+
+Object.freeze(pessoa) //A função .freeze congela a variável não permitindo mais que seu conteúdo seja alterado
+
+pessoa.nome = "Maria" //Se mesmo assim tentar autera-lo o programa simplesmente ignorará essa tentativa de alteração e mantera o conteúdo que já tinha
+
+console.log(pessoa.nome)
+
+const pessoaConstante = Object.freeze({ nome: "Joao" }) //Também é possível criar uma constante com um objeto e já congela-los imediatamente impedindo que seja possível realizar qualquer alteração nesse objeto
+
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
