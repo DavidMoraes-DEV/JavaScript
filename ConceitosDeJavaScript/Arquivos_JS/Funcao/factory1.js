35cf031c66b8ab79a1e7dee6c9bda076dae1cab1
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-21 23:58:43 -0300

    Funcao: Factory parte 01

---
+++ ConceitosDeJavaScript/Arquivos_JS/Funcao/factory1.js
@@ -0,0 +1,31 @@
+// Factory simples
+function criarPessoa() {
+    return {
+        nome: 'Ana',
+        sobrenome: 'Silva'
+    }
+}
+
+console.log(criarPessoa())
+
+//Função Factory é uma função que retorna um novo objeto apartir da chamada dessa função
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
