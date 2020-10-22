00b96018ec5dce880f1182a070b2871f4e459840
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-21 22:32:42 -0300

    Fundamentos: Objeto parte 02

---
+++ ConceitosDeJavaScript/Arquivos_JS/Fundamentos/Objeto02.js
@@ -0,0 +1,29 @@
+console.log(typeof Object) //Mostra o tipo que é o Object
+console.log(typeof new Object) //Mostra o tipo que foi instanciado essa função
+
+const Cliente = function(){} //Constante recebe uma função
+console.log(typeof Cliente) //Atualmente é do tipo função
+console.log(typeof new Cliente) //Como foi instanciado agora é do tipo objeto
+
+class Produto{} //Classe é uma função
+console.log(typeof Produto) //Como foi instanciado um novo Produto agora é do tipo objeto
+
+
+
+
+/*
+================================================================
+=---------------------- FINALIZAR CONSOLE ---------------------=
+================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
