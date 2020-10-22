2cc9f7fe6c57acc7839cfe2a20681f1bfe2cd2b5
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-21 23:27:34 -0300

    Estruturas de Controle: Break Continue

---
+++ ConceitosDeJavaScript/Arquivos_JS/Estruturas_de_Controle/breakContinue.js
@@ -0,0 +1,43 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (x in nums) {
+    if (x == 5) {
+        break //Causa um desvio de fluxo, ou seja, interrompe a sequencia de comandos dentro do escopo que esta contido
+    }
+    console.log(`${x} = ${nums[x]}`)
+}
+
+for (y in nums) {
+    if (y == 5) {
+        continue
+    }
+    console.log(`${y} = ${nums[y]}`)
+}
+
+externo:
+for (a in nums) {
+    for (b in nums) {
+        if(a == 2 && b == 3) break externo
+        console.log(`Par = ${a},${b}`)
+    }
+}
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
