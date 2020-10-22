bb4acb42a47043966c78c887a1366c6382e627f6
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-22 17:19:14 -0300

    Array: Funcao Map parte 03

---
+++ ConceitosDeJavaScript/Arquivos_JS/Array/FuncaoMap03.js
@@ -0,0 +1,47 @@
+//Implementando meu próprio Map para entender como é o funcionamento interno do Map
+Array.prototype.MeuMap = function (callback) {
+    const newArray = []
+    for (let i = 0; i < this.length; i++) {
+        newArray.push(callback(this[i], i, this))
+    }
+    return newArray
+}
+
+const carrinho = [
+    '{"nome": "Borracha", "preco": 3.45 }',
+    '{"nome": "Caderno", "preco": 13.90 }',
+    '{"nome": "Kit de Lapis", "preco": 41.22 }',
+    '{"nome": "Caneta", "preco": 7.50 }'
+]
+
+console.log("\nArray original:")
+console.log(carrinho)
+
+//Retornar um array apenas com os preços
+const paraObjeto = e => JSON.parse(e)
+const apenasPreco = e => e.preco
+
+const resultado = carrinho.MeuMap(paraObjeto).MeuMap(apenasPreco)
+
+console.log("\nNovo array apenas com os preços:")
+console.log(resultado)
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
