8f06c256f175fcc73dfacbc6205a4fa1894e5731
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-22 00:00:20 -0300

    Funcao: Classe vs Factory parte 02

---
+++ ConceitosDeJavaScript/Arquivos_JS/Funcao/classeVsFactory.js
@@ -0,0 +1,41 @@
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome
+    }
+
+    falar() {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa('João')
+p1.falar()
+
+const criarPessoa = nome => {
+    return {
+        falar: () => console.log(`Meu nome é ${nome}`)
+    }
+}
+
+const p2 = criarPessoa('João')
+p2.falar()
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
