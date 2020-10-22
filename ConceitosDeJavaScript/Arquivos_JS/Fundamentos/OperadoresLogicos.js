d0c945f84ae86031c6d16b5454d4b775fe00ca14
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-21 22:46:38 -0300

    Fundamentos: Operadores Logicos

---
+++ ConceitosDeJavaScript/Arquivos_JS/Fundamentos/OperadoresLogicos.js
@@ -0,0 +1,36 @@
+function compras(trabalho1, trabalho2) {
+
+    const comprarSorvete = trabalho1 || trabalho2
+    const comprarTv50 = trabalho1 && trabalho2
+
+    // const comprarTv32 = !!(trabalho1 ^ trabalho2) // bitwise xor
+    const comprarTv32 = trabalho1 != trabalho2
+    const manterSaudavel = !comprarSorvete // operador unário
+
+    return { comprarSorvete, comprarTv50, comprarTv32, manterSaudavel }
+}
+
+console.log(compras(true, true))
+console.log(compras(true, false))
+console.log(compras(false, true))
+console.log(compras(false, false))
+
+
+
+
+/*
+================================================================
+=---------------------- FINALIZAR CONSOLE ---------------------=
+================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
