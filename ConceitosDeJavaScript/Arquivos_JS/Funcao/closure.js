c59ed4e1180d8721f88915beb92768049794a2c0
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-21 23:57:37 -0300

    Funcao: Closure

---
+++ ConceitosDeJavaScript/Arquivos_JS/Funcao/closure.js
@@ -0,0 +1,37 @@
+// Closure é o escopo criado quando uma função é declarada
+// Esse escopo permite a função acessar e manipular variáveis externas à função
+
+// Contexto léxico em ação!
+
+const x = 'Global'
+
+function fora() {
+    const x = 'Local'
+    function dentro() {
+        return x
+    }
+    return dentro
+}
+
+const minhaFuncao = fora()
+console.log(minhaFuncao())
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
