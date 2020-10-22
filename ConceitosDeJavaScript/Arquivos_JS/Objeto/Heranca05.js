4d4e50a24a62c1a9acedc2daa1efc9d12fe2a4d3
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-22 00:17:44 -0300

    Objeto: Heranca parte 05

---
+++ ConceitosDeJavaScript/Arquivos_JS/Objeto/Heranca05.js
@@ -0,0 +1,38 @@
+//Toda Função possui um atributo chama .prototype
+console.log(typeof String)
+console.log(typeof Array)
+console.log(typeof Object)
+
+String.prototype.reverse = function () {
+    return this.split('').reverse().join('')
+}
+
+console.log("Escola Cod3r".reverse())
+
+Array.prototype.first = function() {
+    return this[0]
+}
+
+console.log([1, 2, 3, 4, 5].first())
+console.log(['a', 'b', 'c'].first())
+
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
