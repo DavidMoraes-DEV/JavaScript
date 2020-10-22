8e4178cb635ca7e857aa1f82cd1bf8fe550b84ee
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-22 00:22:11 -0300

    Objeto: Classe parte 01

---
+++ ConceitosDeJavaScript/Arquivos_JS/Objeto/Classe01.js
@@ -0,0 +1,56 @@
+//Em js a classe é convertido em uma função
+class Lancamento { 
+    constructor(nome = "Generico", valor = 0) //Aqui é definido os parâmetros da função construtora
+    {
+        this.nome = nome
+        this.valor = valor
+    }
+}
+
+class CicloFinanceiro {
+    constructor(mes, ano) {
+        this.mes = mes
+        this.ano = ano
+        this.lancamentos = []
+    }
+
+    addLancamentos(...lancamentos) {
+        lancamentos.forEach(l => this.lancamentos.push(l))
+    }
+
+    sumario() {
+        let valorCOnsolidado = 0
+        this.lancamentos.forEach(l => {
+            valorCOnsolidado += l.valor
+        })
+        return valorCOnsolidado
+    }
+}
+
+const salario = new Lancamento("Salario", 4500)
+const contaDeLuz = new Lancamento("Luz", -220)
+
+const contas = new CicloFinanceiro(6, 2018)
+contas.addLancamentos(salario, contaDeLuz)
+console.log(contas.sumario())
+
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
