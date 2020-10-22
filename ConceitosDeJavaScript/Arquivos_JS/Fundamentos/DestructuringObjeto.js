d68f6e370d6ca19da8099b1d0d6b9fe5e5318674
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-21 22:41:15 -0300

    Fundamentos: Destructuring Objeto

---
+++ ConceitosDeJavaScript/Arquivos_JS/Fundamentos/DestructuringObjeto.js
@@ -0,0 +1,40 @@
+//Novo recurso do ES2015
+
+const pessoa = {
+    nome: "Ana",
+    idade: 5,
+    endereco: {
+        logradouro: "Rua ABC",
+        numero: 1000
+    }
+}
+
+const {nome, idade } = pessoa //Nesse contexto as {} representa o operador de destruturação (destructuring) e diz o seguinte: Tire de dentro do objeto "pessoa" as atribuições "nome e idade"
+console.log(nome, idade)
+
+const { nome: n, idade: i} = pessoa
+console.log(n, i)
+
+const {sobrenome, altura = true} = pessoa //Nesse caso se não existir a atribuição altura dentro do objeto pessoa será retornado o valor true como padrão e no caso de sobrenome como não foi definido um valor padrão será retornado o tipo undefined
+
+console.log(sobrenome, altura)
+
+
+
+
+/*
+================================================================
+=---------------------- FINALIZAR CONSOLE ---------------------=
+================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
