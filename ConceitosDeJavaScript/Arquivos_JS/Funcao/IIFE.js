74ca903ad8c0d75443895f87c5b14ee8b8a2ab07
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-22 00:02:23 -0300

    Funcao: IIFE

---
+++ ConceitosDeJavaScript/Arquivos_JS/Funcao/IIFE.js
@@ -0,0 +1,27 @@
+// IIFE -> Immediately Invoked Function Expression
+
+(function() {
+    console.log('Será executado na hora!')
+    console.log('Foge do escopo mais abrangente!')
+})()
+
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
