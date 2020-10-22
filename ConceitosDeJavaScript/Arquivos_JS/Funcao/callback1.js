f6a81fb1a4f26253fdae1188d8d111741b001c2f
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-21 23:51:50 -0300

    Funcao: Callback parte 01

---
+++ ConceitosDeJavaScript/Arquivos_JS/Funcao/callback1.js
@@ -0,0 +1,28 @@
+const fabricantes = ["Mercedes", "Audi", "BMW"]
+
+function imprimir(nome, indice) {
+    console.log(`${indice + 1}. ${nome}`)
+}
+
+fabricantes.forEach(imprimir) //Callback é isso, chama devolta uma determinada função que foi referênciada como parâmetro de outra função
+fabricantes.forEach(fabricante => console.log(fabricante))
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
