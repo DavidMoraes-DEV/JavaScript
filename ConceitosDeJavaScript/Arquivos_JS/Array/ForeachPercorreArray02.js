911c056a7fb457edc1754b7b892900017d4c2f40
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-22 16:11:34 -0300

    Array: Percorrendo array com o forEach parte 02

---
+++ ConceitosDeJavaScript/Arquivos_JS/Array/ForeachPercorreArray02.js
@@ -0,0 +1,35 @@
+//Criando uma função forEach dentro do prototype Array
+Array.prototype.meuForEach = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        callback(this[i], i, this)
+    }
+}
+
+const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
+
+console.log()
+
+//Utilizando a função forEach creada manualmente como um forEach oficial do js
+aprovados.meuForEach(function (nome, indice) {
+    console.log(`${indice + 1}) ${nome}`)
+})
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
