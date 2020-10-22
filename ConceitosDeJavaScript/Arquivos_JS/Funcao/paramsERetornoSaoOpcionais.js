5bce75315188d4c573f833f73f0043fdff4f6a38
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-21 23:36:39 -0300

    Funcao: Parametros e retorno sao opcionais

---
+++ ConceitosDeJavaScript/Arquivos_JS/Funcao/paramsERetornoSaoOpcionais.js
@@ -0,0 +1,34 @@
+function area(largura, altura) {
+    const area = largura * altura
+    if (area > 20) {
+        console.log(`Valor acima do permitido: ${area}m2.`)
+    } else {
+        return area
+    }
+}
+
+console.log(area(2, 2))
+console.log(area(2))
+console.log(area())
+console.log(area(2, 3, 17, 22, 44))
+console.log(area(5, 5))
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
