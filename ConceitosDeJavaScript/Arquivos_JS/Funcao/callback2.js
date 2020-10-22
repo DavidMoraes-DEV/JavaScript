0a826999863338dbcf5135352a83e0b8c2e4cf7e
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-21 23:52:35 -0300

    Funcao: Callback parte 02

---
+++ ConceitosDeJavaScript/Arquivos_JS/Funcao/callback2.js
@@ -0,0 +1,42 @@
+const notas = [7.7, 6.5, 5.2, 8.9, 3.6, 7.1, 9.0]
+
+// Sem callback
+const notasBaixas1 = []
+for (let i in notas) {
+    if (notas[i] < 7) {
+        notasBaixas1.push(notas[i])
+    }
+}
+
+console.log(notasBaixas1)
+
+// Com callback
+const notasBaixas2 = notas.filter(function (nota) { //Função filter de array extrai determinado elemento de dentro do array que atende a um condição
+    return nota < 7
+})
+
+console.log(notasBaixas2)
+
+const notasMenorQue7 = nota => nota < 7
+const notasBaixas3 = notas.filter(notasMenorQue7)
+console.log(notasBaixas3)
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
