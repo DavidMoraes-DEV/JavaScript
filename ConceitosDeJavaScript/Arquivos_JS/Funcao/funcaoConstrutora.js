40cd737b726ff8e1d8da690093de69c7d0786975
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-21 23:55:00 -0300

    Funcao: Construtoras

---
+++ ConceitosDeJavaScript/Arquivos_JS/Funcao/funcaoConstrutora.js
@@ -0,0 +1,51 @@
+function Carro(velocidadeMaxima = 200, delta = 5) {
+    // atributo privado
+    let velocidadeAtual = 0
+
+    // metodo publico
+    this.acelerar = function () {
+        if (velocidadeAtual + delta <= velocidadeMaxima) {
+            velocidadeAtual += delta
+        } else {
+            velocidadeAtual = velocidadeMaxima
+        }
+    }
+
+    // metodo publico
+    this.getVelocidadeAtual = function () {
+        return velocidadeAtual
+    }
+}
+
+const uno = new Carro
+uno.acelerar()
+console.log(uno.getVelocidadeAtual())
+
+const ferrari = new Carro(350, 20)
+ferrari.acelerar()
+ferrari.acelerar()
+ferrari.acelerar()
+console.log(ferrari.getVelocidadeAtual())
+
+console.log(typeof Carro)
+console.log(typeof ferrari)
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
