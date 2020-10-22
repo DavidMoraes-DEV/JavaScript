74851045945e1aa9a57e91528ccfb1978ae666c9
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-21 22:11:59 -0300

    Fundamentos: JS Tipagem Fraca

---
+++ ConceitosDeJavaScript/Arquivos_JS/Fundamentos/TipagemFraca.js
@@ -0,0 +1,41 @@
+//A linguagem JavaSript possui tipagem Fraca
+
+let teste = 'Legal';
+
+console.log("\nConteúdo atual da variável (teste): " + teste);
+console.log("A variável (teste) é do tipo: " + typeof teste); //typeof Mostra qual é o tipo atual da variável (number, string, etc...)
+
+teste = 3.1516
+
+console.log("\nConteúdo atual da variável (teste): " + teste);
+console.log("A variável (teste) é do tipo: " + typeof teste);
+
+teste = 'D'
+
+console.log("\nConteúdo atual da variável (teste): " + teste);
+console.log("A variável (teste) é do tipo: " + typeof teste);
+
+//Evitar nomes genéricos e siglas que não deixa claro sua principal funcionalidade como por exemplo:
+let valor = '' //Valor de que?
+let numero = 1 //Número de que?
+let pqp = false //Significa: Produto Químico Perigoso. Hammm? KKKK
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
