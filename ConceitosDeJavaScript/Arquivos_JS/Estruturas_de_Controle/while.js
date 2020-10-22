82ac483f407308b87208566804006c38ec22e704
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-21 23:10:49 -0300

    Estruturas de Controle: While

---
+++ ConceitosDeJavaScript/Arquivos_JS/Estruturas_de_Controle/while.js
@@ -0,0 +1,33 @@
+function getInteiroAleatorioEntre(min, max) {
+    const valor = Math.random() * (max - min) + min
+    return Math.floor(valor)
+}
+
+let opcao = 0
+
+while (opcao != -1) {
+    opcao = getInteiroAleatorioEntre(-1, 10)
+    console.log(`Opção escolhida foi ${opcao}.`)
+}
+
+console.log('Até a próxima!')
+
+
+
+
+/*
+==================================================================
+==---------------------- FINALIZAR CONSOLE ---------------------==
+==================================================================
+*/
+let readline = require('readline');
+let resp = "";
+
+let leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+leitor.question("\n\nPress Enter to Exit...", function (answer) {
+    leitor.close();
+});
