af239848fecf5d222af1d0fb40b5f569345bd16d
DavidMoraes-DEV <investtraderrs@gmail.com>
2020-10-23 13:58:51 -0300

    Node: Arquivo package.json

---
+++ ConceitosDeJavaScript/Arquivos_JS/Node/Funcionarios/ArquivoPackage_json.js
@@ -0,0 +1,5 @@
+/*
+O arquivo package.json tem como uma de suas funcionalidades conter a descrição do projeto e também salvar todas as
+descrições de dependencias necessárias para o projeto, na chave "dependencies" que coloca
+o nome da dependencia e sua versão.
+*/
---
+++ ConceitosDeJavaScript/Arquivos_JS/Node/Funcionarios/Funcionarios.js
@@ -0,0 +1,17 @@
+const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
+const axios = require('axios')
+
+axios.get(url).then(response => {
+    const funcionarios = response.data
+    console.log(funcionarios)
+})
+
+
+
+/*
+================================================================
+=---------------------- FINALIZAR CONSOLE ---------------------=
+================================================================
+*/
+const Console = require('../../../app')
+Console.Finalizar()
